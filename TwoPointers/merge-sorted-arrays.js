f6e9094f287fcf0b635fa0de35ca0030ee95ffa8
cotne <cotne.sxvitaridze@gmail.com>
2025-12-06 11:27:55 +0400

    Add merge-sorted-arrays.js and remove multiple outdated array manipulation files

--- TwoPointers/merge-sorted-arrays.js
+++ TwoPointers/merge-sorted-arrays.js
@@ -1,3 +1,10 @@
+// Difficulty: EASY
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+// You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+
+// Solution Big O(m + n) Time O(1) Space.
+// Execution time: 68 ms, faster than 99.36% of JavaScript online submissions for Merge Sorted Array.
 var merge = function(nums1, m, nums2, n) {
     let i = m - 1; // Pointer for nums1
     let j = n - 1; // Pointer for nums2
@@ -23,9 +30,9 @@ var merge = function(nums1, m, nums2, n) {
     }
 };
 
+// Example usage:
 nums1 = [1, 2, 3, 0, 0, 0], m = 3
 nums2 = [2, 5, 6], n = 3
 
 merge(nums1, m, nums2, n)
-
 console.log(nums1)
